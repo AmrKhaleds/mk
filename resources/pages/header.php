9beff16bb6e79050dd6a49247f125306771527c8
akeissa <dev.amrkhaled@gmail.com>
2023-03-03 23:40:26 +0200

    pages

---
+++ resources/pages/header.php
@@ -0,0 +1,43 @@
+    <!-- ======= Header ======= -->
+    <header id="header">
+        <div class="container">
+
+            <h1><a href="index.html">Mahmoud Khaled</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+            <h2>I'm a passionate <span>Videographer</span> from Alexandria</h2>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link active" href="#header">Home</a></li>
+                    <li><a class="nav-link" href="#about">About</a></li>
+                    <li><a class="nav-link" href="#resume">Resume</a></li>
+                    <li><a class="nav-link" href="#services">Services</a></li>
+                    <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
+                    <li><a class="nav-link" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+            <!-- .navbar -->
+
+            <div class="social-links">
+                <a href="https://www.facebook.com/Mahmoud.videography" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/mahmoud.videography/" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.youtube.com/@mahmoudkhaled2659" class="youtube"><i class="bi bi-youtube"></i></a>
+                <a href="https://www.tiktok.com/@mahmoudkhaled1274" class="tiktok"><i class="bi bi-tiktok"></i></a>
+            </div>
+
+        </div>
+    </header>
+    <!-- End Header -->
+
+
+
+
+
+
+
+
+
+
+
