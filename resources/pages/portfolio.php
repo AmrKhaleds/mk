64b27320c0d74bf7130e0064dc817256ca90a669
akeissa <dev.amrkhaled@gmail.com>
2023-03-04 09:52:03 +0200

    Modify gallary

--- resources/pages/portfolio.php
+++ resources/pages/portfolio.php
@@ -13,8 +13,8 @@
                     <ul id="portfolio-flters">
                         <li data-filter="*" class="filter-active">All</li>
                         <li data-filter=".filter-photo">PHOTO</li>
-                        <li data-filter=".filter-card">Card</li>
-                        <li data-filter=".filter-web">Web</li>
+                        <!-- <li data-filter=".filter-card">Card</li>
+                        <li data-filter=".filter-web">Web</li> -->
                     </ul>
                 </div>
             </div>
@@ -23,10 +23,10 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%85%D8%A7%D8%A1%20%D8%A7%D9%84%D8%B0%D9%87%D8%A8/6H8A0017-1024x1536.jpg" class="img-fluid" alt="">
+                        <img  src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%85%D8%A7%D8%A1%20%D8%A7%D9%84%D8%B0%D9%87%D8%A8/6H8A0017-1024x1536.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>App 1</h4>
-                            <p>App</p>
+                            <h4>ماء الدهب</h4>
+                            <p>Perfume Brand</p>
                             <div class="portfolio-links">
                                 <a href="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%85%D8%A7%D8%A1%20%D8%A7%D9%84%D8%B0%D9%87%D8%A8/6H8A0017-1024x1536.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
                                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -37,10 +37,10 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%87%D8%A7%D8%AF%D9%8A/WhatsApp%20Image%202023-01-16%20at%2010.19.31%20PM%20(2).jpeg" class="img-fluid" alt="">
+                        <img   src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%87%D8%A7%D8%AF%D9%8A/WhatsApp%20Image%202023-01-16%20at%2010.19.31%20PM%20(2).jpeg" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
+                            <h4>Hady</h4>
+                            <p>Hady Brand</p>
                             <div class="portfolio-links">
                                 <a href="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/%D9%87%D8%A7%D8%AF%D9%8A/WhatsApp%20Image%202023-01-16%20at%2010.19.31%20PM%20(2).jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -51,10 +51,10 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/Zoom%2011/1015%20(4).jpg" class="img-fluid" alt="">
+                        <img   src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/Zoom%2011/1015%20(4).jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>App 2</h4>
-                            <p>App</p>
+                            <h4>Zoom</h4>
+                            <p>Zoom Brand</p>
                             <div class="portfolio-links">
                                 <a href="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/Zoom%2011/1015%20(4).jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
                                 <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
@@ -63,9 +63,23 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-photo">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+                        <img   src="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/Trible%20A/3I2A9246-1536x1024.jpg" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Trible A</h4>
+                            <p>Trible A Brand</p>
+                            <div class="portfolio-links">
+                                <a href="https://mahmoudkhaled.org/old/wp-content/uploads/2023/01/mk/Trible%20A/3I2A9246-1536x1024.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
+                                <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <div class="portfolio-wrap">
+                        <img   src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Card 2</h4>
                             <p>Card</p>
@@ -77,23 +91,9 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                    <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
-                        <div class="portfolio-info">
-                            <h4>Web 2</h4>
-                            <p>Web</p>
-                            <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+                        <img   src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>App 3</h4>
                             <p>App</p>
@@ -107,7 +107,7 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+                        <img   src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Card 1</h4>
                             <p>Card</p>
@@ -121,7 +121,7 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+                        <img   src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Card 3</h4>
                             <p>Card</p>
@@ -135,7 +135,7 @@
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                     <div class="portfolio-wrap">
-                        <img loading="lazy" src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+                        <img   src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Web 3</h4>
                             <p>Web</p>
@@ -145,7 +145,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
             </div>
 
