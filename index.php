071cee9e2af5fffaeac19aa1e489416705d432c4
akeissa <dev.amrkhaled@gmail.com>
2023-03-04 11:23:39 +0200

    meta

--- index.php
+++ index.php
@@ -1,51 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <title>Mahmoud Khaled</title>
-        <meta content="" name="description">
-        <meta content="" name="keywords">
-        <!-- Favicons -->
-        <link href="assets/img/favicon.png" rel="icon">
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-        <!-- Vendor CSS Files -->
-        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Mahmoud Khaled</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-        <!-- Template Main CSS File -->
-        <link href="assets/css/style.css" rel="stylesheet">
-    </head>
 
-    <body>
-        <?php 
-            require_once './resources/pages/header.php';
-            require_once './resources/pages/about.php';
-            require_once './resources/pages/resume.php';
-            require_once './resources/pages/services.php';
-            require_once './resources/pages/portfolio.php';
-            require_once './resources/pages/contact.php';
-        ?>
-        <!-- Vendor JS Files -->
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="./assets/js/isotope.pkgd.js"></script>
-        <script src="./assets/js/packery-mode.pkgd.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
+    <meta name="keywords" content="mahmoud, khaled, mahmoudkhaled, videography, photography, egypt, videography in egypt, video agency in egypt, photo agency in egypt"/>
+    <meta name="description" content=""/>
+    <meta name="subject" content="A warm welcome to enter my Website, Do you want Professional video and photo for your brand.... you in the right place(:">
+    <meta name="copyright"content="Mahmoud Khaled">
+    <meta name="language" content="en">
+    <meta name="robots" content="index,follow" />
+    <meta name="revised" content="Sat, March 4th, 2023, 11:00 AM" />
+    <meta name="Classification" content="Business">
+    <meta name="author" content="Mahmoud Khaled, contact@mahmoudkhaled.org">
+    <meta name="designer" content="Amr Khaled">
+    <meta name="copyright" content="mahmoudkhaled">
+    <meta name="reply-to" content="contact@mahmoudkhaled.org">
+    <meta name="owner" content="Mahmoud Khaled">
+    <meta name="url" content="https://mahmoudkhaled.org">
+    <meta name="identifier-URL" content="https://mahmoudkhaled.org">
+    <meta name="category" content="Videography">
+    <meta name="coverage" content="Egypt">
+    <meta name="distribution" content="Global">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache">
 
-        <!-- Template Main JS File -->
-        <script src="assets/js/main.js"></script>
+    <meta name="og:title" content="Mahmoud Khaled"/>
+    <meta name="og:type" content="video"/>
+    <meta name="og:url" content="http://www.imdb.com/title/tt0117500/"/>
+    <meta name="og:image" content="./assets/img/logo(dark).png"/>
+    <meta name="og:site_name" content="Mahmoud Khaled"/>
+    <meta name="og:description" content="A warm welcome to enter my Website, Do you want Professional video and photo for your brand.... you in the right place(: "/>
 
-    </body>
+    <meta name="fb:page_id" content="Mahmoud.videography" />
+
+    <meta name="og:email" content="contact@mahmoudkhaled.org"/>
+    <meta name="og:phone_number" content="+201110012666"/>
+    <!-- <meta name="og:fax_number" content="+1-415-123-4567"/> -->
+
+    <meta name="og:latitude" content="37.416343"/>
+    <meta name="og:longitude" content="-122.153013"/>
+    <meta name="og:street-address" content="Cairo - Alexandria Desert Rd, Qetaa at Tarik as Sahrawi, Amreya 1, Alexandria Governorate"/>
+    <meta name="og:locality" content="Egypt"/>
+    <meta name="og:region" content="EG"/>
+    <meta name="og:country-name" content="EG"/>
+
+
+    <!-- Favicons -->
+    <link href="./assets/img/logo.png" rel="icon" media="(prefers-color-scheme: light)" />
+    <link href="./assets/img/logo(dark).png" rel="icon" media="(prefers-color-scheme: dark)" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    require_once './resources/pages/header.php';
+    require_once './resources/pages/about.php';
+    require_once './resources/pages/resume.php';
+    require_once './resources/pages/services.php';
+    require_once './resources/pages/portfolio.php';
+    require_once './resources/pages/contact.php';
+    ?>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="./assets/js/isotope.pkgd.js"></script>
+    <script src="./assets/js/packery-mode.pkgd.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
 
 </html>
